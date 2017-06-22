d3ea653e65dbe21155d18811c9a7b7160eb2ee51
Oleg <rich007rich007rich@mail.ru>
2017-06-22 21:37:08 +0300

    commit

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-$('.arrow-block').click(function(){
-	$('.over-main').css('margin-left', '-1920px');
-})
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
